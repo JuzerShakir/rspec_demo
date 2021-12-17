22b602674b0368473bf255333fdfeb81baffad64
Juzer Shakir <juzershakir.webdev@gmail.com>
2021-12-17 11:10:42 +0530

    Update home/index route to root

--- spec/features/home_spec.rb
+++ spec/features/home_spec.rb
@@ -3,23 +3,23 @@ require 'rails_helper'
 RSpec.describe 'Home features' do
     describe 'Index' do
         it 'should have title' do
-            visit('home/index')
+            visit('/')
             expect(page).to have_content('Smartphones')
         end
 
         it 'should have url path' do
-            visit('home/index')
-            expect(current_path).to eql('/home/index')
+            visit('/')
+            expect(current_path).to eql('/')
         end
 
         it 'should have content' do
-            visit('home/index')
+            visit('/')
             click_link('About')
             expect(page).to have_content('About')
         end
 
         it 'must have a link to about page' do
-            visit('home/index')
+            visit('/')
             click_link('About')
             expect(current_path).to eql('/about')
         end
